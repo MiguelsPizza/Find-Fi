816873fc899cb830ab1b9c00623f8ba2bcb8fa12
MiguelsPizza <alexmnahas@gmail.com>
2022-04-15 08:35:28 -0400

    migrated the project into a new repo and seperated the client side and sever side so I could use vite to server the files

---
+++ src/MainPage.jsx
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from "react";
+import MainB from "./RSSI/MainB.jsx";
+import TopBar from "./RSSI/TopBar.jsx";
+import SideBar from "./RSSI/SideBar/SideBar.jsx";
+import {
+  getFirestore,
+  collection,
+  query,
+  doc,
+  setDoc,
+  getDoc,
+} from "firebase/firestore";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Nav from "react-bootstrap/Nav";
+import WifiMap from "./RSSI/SideBar/WifiMap.jsx";
+import FriendsList from "./RSSI/SideBar/FriendsList.jsx";
+
+
+
+
+const MainPage = ({ auth, firestore }) => {
+  const [networks, setNetworks] = useState([]);
+  const [autoFetch, ToggleAutoFetch] = useState(false);
+  const [knownNetworks, setKnowNetworks] = useState([]);
+  const[currentTab, setTab] = useState("SideBar")
+
+  const updateNetworksData = (data) => {
+    console.log('data', data)
+    if (data) {
+      setNetworks(data);
+    } else {
+      fetch("http://localhost:8550/currentConections",{
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json'
+        },
+      })
+        .then((response) => response.json())
+        .then((dataList) => setNetworks(dataList));
+    }
+  };
+  useEffect(() => {
+    console.log("autoFetch", autoFetch);
+    if (autoFetch) {
+      let eventSource = new EventSource("http://localhost:8550/auto");
+      eventSource.onmessage = (e) => updateNetworksData(JSON.parse(e.data));
+
+
+      eventSource.onerror = () => {
+        // error log here
+
+        eventSource.close();
+      };
+      return () => {
+        eventSource.close();
+      };
+    } else {
+      console.log("here");
+      updateNetworksData(null);
+    }
+  }, [autoFetch]);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const db = getFirestore();
+      const docRef = doc(db, "Users", auth.currentUser.uid);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        setKnowNetworks(docSnap.data().networks);
+      } else {
+        // doc.data() will be undefined in this case
+        const userData = {
+          UID: auth.currentUser.uid,
+          email: auth.currentUser.email,
+          userName: auth.currentUser.displayName,
+        };
+        await setDoc(doc(db, "Users", auth.currentUser.uid), userData, {
+          merge: true,
+        });
+        console.log("added user");
+      }
+    };
+    getUserData();
+  }, [auth]);
+
+  return (
+    <Container className="bg-light" style={{ height: "1500px", width: "100%" }}>
+      <Row>
+        <TopBar />
+      </Row>
+      <Row>
+        <MainB />
+      </Row>
+      <Row>
+        <Nav fill variant="tabs" defaultActiveKey="link-0">
+          <Nav.Item>
+            <Nav.Link eventKey="link-0" onClick={() =>{setTab("SideBar")}}>AvalibleNetworks</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1" onClick={() =>{setTab("FriendsList")}}>Friends</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-2" onClick={() =>{setTab("WifiMap")}}>Map</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-3" >MyNetworks</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Row>
+      <Row>
+        { currentTab ==="SideBar" && <SideBar
+          networks= {networks}
+          ToggleAutoFetch={ToggleAutoFetch}
+          autoFetch={autoFetch}
+          updateNetworksData={updateNetworksData}
+          knownNetworks={knownNetworks}
+        />}
+                { currentTab ==="WifiMap" && <WifiMap
+          networks= {networks}
+
+          updateNetworksData={updateNetworksData}
+          knownNetworks={knownNetworks}
+        />}
+                { currentTab ==="FriendsList" && <FriendsList
+          networks= {networks}
+          auth={auth}
+
+          updateNetworksData={updateNetworksData}
+          knownNetworks={knownNetworks}
+        />}
+
+      </Row>
+    </Container>
+  );
+};
+//md={{ span: 3, offset: 3 }}
+
+export default MainPage;
---
+++ src/RSSI/SideBar/FriendsList.jsx
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from "react";
+import FriendsListItem from "./FriendsListItem.jsx";
+import Accordion from "react-bootstrap/Accordion";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import styled from "styled-components";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import AddNetworkModal from "./AddNetworkModal.jsx";
+import {
+  getFirestore,
+  collection,
+  query,
+  doc,
+  setDoc,
+  getDoc,
+} from "firebase/firestore";
+import ListGroup from "react-bootstrap/ListGroup";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+
+// const cityRef = doc(db, 'cities', 'BJ');
+// setDoc(cityRef, { capital: true }, { merge: true });
+
+const StyledDiv = styled.div`
+&&& {
+  max-height: 1000px; /* or any value */
+  overflow-y: auto;
+}
+`;
+const Styledh3 = styled.h3`
+&&& {
+  color: grey;
+  font-size: 20px;
+}
+`;
+//
+function FriendsList({ networks, auth, updateNetworksData, knownNetworks }) {
+  const [rememberAuto, setRemeberAuto] = useState(null);
+  const [networkToAdd, setnetworkToAdd] = useState(false);
+  console.log("modal toggle", !!networkToAdd);
+  const db = getFirestore();
+  let counter = 0;
+  // ${getBackgroundColor(network.quality)}
+
+  const decideToggleFetch = () => {
+    if (rememberAuto) {
+      ToggleAutoFetch(true);
+    }
+  };
+  const [friendsList, setFriendsList] = useState({});
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const getFriendsList = async () => {
+      const docRef = doc(db, "Users", auth.currentUser.uid);
+      const docSnap = await getDoc(docRef);
+      const arr = [];
+      console.log("friends", docSnap.data());
+      const userData = docSnap.data();
+      setFriendsList(userData.friends);
+      userData.friends.forEach((friend) => {
+        const doStuff = async () => {
+          const id = friend.trim();
+          console.log("id", id);
+          const docFriend = doc(db, "Users", id);
+          const docFriendSnap = await getDoc(docFriend);
+          const temp = docFriendSnap.data();
+          console.log("docFriendSnap.data()", docFriendSnap.data());
+          setData(temp);
+        };
+        doStuff();
+      });
+    };
+    getFriendsList();
+  }, []);
+  const [requestedacc, setAcc] = useState("Request Accsess");
+  const [event, setEvent] = useState(0);
+
+  const test = async () => {
+    const body = {};
+    const serverResponse = await fetch("http://localhost:8550/request", {
+      method: "POST",
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(body),
+    });
+  };
+  // console.log('auth', auth)
+
+  //onMouseLeave={() => decideToggleFetch}
+  return (
+    <Container style={{ width: "100%" }}>
+      <Navbar bg="dark" variant="dark">
+        <AddNetworkModal
+          networkToAdd={networkToAdd}
+          setnetworkToAdd={setnetworkToAdd}
+        />
+        <Container>
+          <Navbar.Brand>
+            <Styledh3>FriendsList</Styledh3>
+          </Navbar.Brand>
+          <Navbar.Text className="justify-content-end">
+            <Row>
+              <Col md="auto">
+                <Button onClick={() => updateNetworksData(null)}>
+                  {networks.length > 0 ? "Refresh" : "Loading..."}
+                </Button>
+              </Col>
+            </Row>
+          </Navbar.Text>
+        </Container>
+      </Navbar>
+      <StyledDiv>
+        <Accordion>
+          <Accordion.Item eventKey={event}>
+            <Accordion.Header>{data && data?.username}</Accordion.Header>
+            <Accordion.Body>
+              <ListGroup horizontal>
+                <ListGroup.Item>
+                  {data && data?.networks[0].ssid}
+                </ListGroup.Item>
+              </ListGroup>
+              <ButtonGroup aria-label="Basic example">
+                <Button
+                  variant="secondary"
+                  onClick={() => {
+                    setAcc("Requested");
+                    test();
+                    setEvent(1);
+                  }}
+                >
+                  {requestedacc}
+                </Button>
+              </ButtonGroup>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </StyledDiv>
+    </Container>
+  );
+}
+//{networkToAdd && <addNetworkModal networkToAdd={networkToAdd}/>}
+// console.log('addNetworkModal', AddNetworkModal)
+
+export default FriendsList;
